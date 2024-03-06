e19c268f132edf3ea616d5f0e99a51af0834860a
Siddiqjon <kuziboevsiddiqjon98@gmail.com>
2024-03-06 18:10:25 +0500

    commit done some features

--- FaceFacts/PeopleView.swift
+++ FaceFacts/PeopleView.swift
@@ -6,10 +6,40 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct PeopleView: View {
+    @Environment(\.modelContext) var modelContext
+    @Query var people: [Person]
+    
+
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            ForEach(people) { person in
+                NavigationLink(value: person) {
+                    Text(person.name)
+                }
+            }
+            .onDelete(perform: deletePeople)
+        }
+    }
+    
+    init(searchString: String = "", sortOrder: [SortDescriptor<Person>] = []) {
+        _people = Query(filter: #Predicate { person in
+            if searchString.isEmpty {
+                true
+            } else {
+                person.name.localizedStandardContains(searchString) || person.emailAddress.localizedStandardContains(searchString) || person.details.localizedStandardContains(searchString)
+            }
+        }, sort: sortOrder)
+    }
+    
+    func deletePeople(at offsets: IndexSet) {
+        for offset in offsets {
+            let person = people[offset]
+            modelContext.delete(person)
+        }
     }
 }
 
