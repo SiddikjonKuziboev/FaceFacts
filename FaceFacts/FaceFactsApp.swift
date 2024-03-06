9d4e568c1e4d2abd20cd262cc6d042c90f15c538
Siddiqjon <kuziboevsiddiqjon98@gmail.com>
2024-03-06 09:51:38 +0500

    Initial Commit

---
+++ FaceFacts/FaceFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceFactsApp.swift
+//  FaceFacts
+//
+//  Created by Siddikjon Kuziboev on 06/03/24.
+//
+
+import SwiftUI
+
+@main
+struct FaceFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
